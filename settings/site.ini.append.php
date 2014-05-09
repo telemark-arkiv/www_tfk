62f6bd52207509e08d33db1280fffb250bb963c9
Geir Gåsodden <geir.gasodden@pythonia.no>
2014-05-09 12:49:39 +0200

    Kills all caching

--- settings/site.ini.append.php
+++ settings/site.ini.append.php
@@ -5,4 +5,25 @@
 [RegionalSettings]
 TranslationExtensions[]=www_tfk
 
+### DEVELOPMENT ONLY ###
+[MailSettings]
+Transport=file
+[DebugSettings]
+DebugOutput=enabled
+DebugRedirection=disabled
+DebugToolbar=disabled
+DebugByIP=disabled
+DebugByUser=disabled
+[TemplateSettings]
+TemplateCompile=disabled
+TemplateCache=disabled
+DevelopmentMode=disabled
+ShowUsedTemplates=enabled
+Debug=disabled
+ShowXHTMLCode=disabled
+[ContentSettings]
+ViewCaching=disabled
+[DatabaseSettings]
+SQLOutput=disabled
+
 */ ?>
