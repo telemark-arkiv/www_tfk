ed9e518631d62d1734b6055a68eed06fbd347078
Geir Gåsodden <geir.gasodden@pythonia.no>
2014-05-12 12:32:11 +0200

    Adds new responsive img sizes

--- settings/image.ini.append.php
+++ settings/image.ini.append.php
@@ -1,10 +1,45 @@
 <?php /* #?ini charset="utf-8"?
 
 [AliasSettings]
+AliasList[]=responsive_12
+AliasList[]=responsive_09
+AliasList[]=responsive_06
+AliasList[]=responsive_04
+AliasList[]=responsive_03
+AliasList[]=responsive_02
+AliasList[]=responsive_01
 AliasList[]=listebilde
 AliasList[]=ingress_smal
 AliasList[]=ingress_bred
 AliasList[]=fullskjerm
+
+[responsive_12]
+Reference=reference
+Filters[]=geometry/scalewidthdownonly=1140
+
+[responsive_09]
+Reference=reference
+Filters[]=geometry/scalewidthdownonly=846
+
+[responsive_06]
+Reference=reference
+Filters[]=geometry/scalewidthdownonly=560
+
+[responsive_04]
+Reference=reference
+Filters[]=geometry/scalewidthdownonly=360
+
+[responsive_03]
+Reference=reference
+Filters[]=geometry/scalewidthdownonly=260
+
+[responsive_02]
+Reference=reference
+Filters[]=geometry/scalewidthdownonly=162
+
+[responsive_01]
+Reference=reference
+Filters[]=geometry/scalewidthdownonly=65
 
 [listebilde]
 Reference=reference
